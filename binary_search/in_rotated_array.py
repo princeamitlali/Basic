ee25d080d0a82c3e5100c2d0e9ec3f415aab3e75
Prince Amit <princeamitlali@gmail.com>
2022-09-20 18:05:29 +0530

    Update in_rotated_array.py

--- binary_search/in_rotated_array.py
+++ binary_search/in_rotated_array.py
@@ -6,23 +6,23 @@ def binary_search(li,to_find):
     
     start = 0
     end = len(li) -1
-    
+
     while end >= start:
         mid = (end + start ) //2
         if li[mid] == to_find :
             print("found")
             return mid
-        if li[start] < li[mid]:
-            if li[start] < to_find and to_find < li[mid]:
+        if li[start] <= li[mid]:
+            if li[start] <= to_find and to_find <= li[mid]:
                 end = mid -1
             else:
                 start = mid + 1
         else:
-            if to_find > li[mid] and to_find<li[end]:
+            if to_find >= li[mid] and to_find <= li[end]:
                 start = mid + 1
             else:
                 end = mid -1
-            
+
     return -1
 
 val = binary_search(li,to_find)
